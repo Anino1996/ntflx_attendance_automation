487848df97ef85b963ade5fc34a520e42f48eb51
Albert Aninagyei Ofori <anino1996.AO@gmail.com>
2021-04-23 03:44:26 -0400

    Update ntflxLogin.py

--- ntflxLogin.py
+++ ntflxLogin.py
@@ -7,12 +7,16 @@ from dotenv import load_dotenv
 from survey import *
 # imort time
 
+# Load environmental variables
 load_dotenv('.env')
 
 def main():
+	
+	# Prevent chrome browser window from showing
 	options=webdriver.ChromeOptions()
 	options.headless=True
 
+	# Change to project directory (Takes care of directory issues when running file directory)
 	if os.environ['LOGNAME']=='anino1996':
 		os.chdir('/Users/anino1996/Python/attendance_automation')
 		DRIVER_PATH=os.path.join(os.getcwd(),'chromedriver')
@@ -25,7 +29,7 @@ def main():
 
 	# DRIVER_PATH='/Users/anino1996/Python/attendance_automation/chromedriver'
 	
-	
+	# Load login creds
 	cred=json.loads(os.getenv('NTFLXLOGIN'))
 
 	mail=cred.get('mail', None)
@@ -33,8 +37,10 @@ def main():
 
 	# browserDriver=webdriver.Chrome(executable_path=DRIVER_PATH,options=options)
 
+	# Initialize chrome browser driver
 	browserDriver.get("https://www.bootcampspot.com")
 	
+	# Log in to website
 	if browserDriver.current_url[-5:]=="login":
 		emailElement=browserDriver.find_element_by_name("emailAddress")
 		emailElement.send_keys(mail)
@@ -52,8 +58,13 @@ def main():
 				
 			
 			else:
+			
+				# Start survey if survey page is the next page after login
+				
 				print("Login Successful!")
 				if 'feedback' in browserDriver.current_url:
+					
+					# Complete the survey and proceed to check in
 					survey_started=start_survey(browserDriver)
 					if survey_started:
 						survey_fdbck=complete_survey(browserDriver)
@@ -65,6 +76,8 @@ def main():
 				if check_in_element:
 					fdbck=check_into_class(check_in_element, browserDriver)
 			print(fdbck)
+			
+			# Send email and log result of check-in attempt
 			communicate(fdbck)
 				
 if __name__=='__main__':
